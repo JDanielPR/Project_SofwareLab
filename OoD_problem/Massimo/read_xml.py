35b543dd02f18619f5e3387cc3e1b118008bb541
IIIaxS <msferza@gmail.com>
2016-05-12 18:41:18 +0200

    read_xml output
    now running the read_xml you get the read data

--- OoD_problem/Massimo/read_xml.py
+++ OoD_problem/Massimo/read_xml.py
@@ -98,20 +98,11 @@ proper .xml file"""
 
 if __name__ == "__main__":
     struct = read_xml()
-    if True:
-        struct.path_list[2].solve()
-        struct.path_list[2].print_solution_list()
-    else:
-        for path in struct.path_list:
-            if type(path) is lp.Loadpath:
-                if not path.component_list:
-                    print("Loadpath", path.id, "is empty")
-                else:
-                    print("Loadpath", path.id, "has these components:")
-                    for comp in path.component_list:
-                        comp.print_info("\t")
+    for path in struct.path_list:
+        if not path.component_list:
+            print("Loadpath", path.id, "is empty")
+        else:
+            print("Loadpath", path.id, "has these components:")
+            for comp in path.component_list:
+                comp.print_info("\t")
 
-            elif type(path) is cp.Connectionpath:
-                print("A connectionpath has been created with these components:")
-                for comp in path.component_list:
-                    comp.print_info("\t")
