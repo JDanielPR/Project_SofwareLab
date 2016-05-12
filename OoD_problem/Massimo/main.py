48988e494518d3293c1e32f9bd0cbee3bff34ee6
IIIaxS <msferza@gmail.com>
2016-05-12 17:37:16 +0200

    create main
    create a main.py file to call the functions, that solve the problem and
    output the solution, in an orderly way

---
+++ OoD_problem/Massimo/main.py
@@ -0,0 +1,21 @@
+from read_xml import read_xml
+
+# create a structure reading the input data
+struct = read_xml()
+
+# solve each path independently
+for path in struct.path_list:
+    path.solve()
+    # now path.solution_list is a list created from an itertools.permutations
+    # object,  which is an iterator:
+    # each member of this iterator is a possible solution
+    
+    # each solution is a tuple object, which contains objects of type
+    # component.Component
+
+    print("solution of path",path.id,":")
+    for index, solution in enumerate(path.solution_list):
+        path.print_solution(index)
+    print("")
+
+# combine the path solution to get the structure solution 
