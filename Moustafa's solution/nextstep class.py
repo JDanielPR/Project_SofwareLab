d9e46396c9f439aa568dcd7a5449d86d33425e6b
moustafabachar <m.b.alsayedahmad@gmail.com>
2016-05-13 23:33:41 +0200

    Python code associated with the solution
    Here is posted the python code I developed for the solution of the 1st
    problem in our project. I am still working on the development

---
+++ Moustafa's solution/nextstep class.py
@@ -0,0 +1,35 @@
+import element as elem
+
+class nextstep():
+  
+  def __int__(self,etree):
+    self.elementtree = etree
+
+  def treetailoring(self):
+    for i in len(elementtree):
+      for j in range(len(i)):
+        if i[j] == False:
+          elementtree.pop(i)
+
+  def carryon(self):
+
+    for i in elementtree:
+
+      counter = 0
+    
+      #determining the motion to be carried out perform it
+      deformotion = 1111111
+      for j in range(2):
+        if i[j].dlength < deformotion:
+          deformotion = i[j].dlength
+      print(deformotion)
+
+      #perform the deformation upon the elements and change the position and state of all the elements
+      for j in range(2):
+        i[j].deform(deformotion)
+        print(i[j].calLength())
+        if i[j].calLength() <= (i[j].length-i[j].dlength):
+          i[j].changestate(False)
+        print(i[j].state)
+
+    
---
+++ Moustafa's solution/node class.py
@@ -0,0 +1,5 @@
+class node:
+    def __node__(self,point1,point2):
+        self.x1 = point1
+        self.x2 = point2
+        
