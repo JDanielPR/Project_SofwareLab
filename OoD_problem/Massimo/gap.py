23b8ed53925810ca22792f88354c2bde915aeaa3
IIIaxS <msferza@gmail.com>
2016-05-14 23:43:01 +0200

    remove old solution
    Since I’m not able to solve merge conflicts, this time this will be the
    way

--- OoD_problem/Massimo/gap.py
+++
@@ -1,4 +0,0 @@
-class Gap(Member):
-    def __init__(self):
-        self.is_a_gap = True
-        
--- OoD_problem/Massimo/loadpath.py
+++
@@ -1,4 +0,0 @@
-class Loadpath:
-    def __init__(self):
-        self.member_list = [ ]
-        self.solution_list = [ ]
