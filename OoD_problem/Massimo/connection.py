ec6310461043fd9d03f94211af824b4b9cadec13
IIIaxS <msferza@gmail.com>
2016-05-17 16:46:46 +0200

    modify constructor of class Connection

--- OoD_problem/Massimo/connection.py
+++ OoD_problem/Massimo/connection.py
@@ -1,6 +1,10 @@
 import component as c
 
 class Connection(c.Component):
-    def __init__(self):
-        c.Component.__init__(self)        
-        pass
+    def __init__(self, name,
+                 node1, node2,
+                 deformable_length, deformable_ratio):
+        c.Component.__init__(self, name,
+                             node1, node2,
+                             deformable_length, deformable_ratio)        
+
