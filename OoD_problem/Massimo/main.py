ab21810f0869015d1bc4a5b1ccc9ecc0e8448568
IIIaxS <msferza@gmail.com>
2016-05-19 00:06:00 +0200

    changes in main.py + .pyc

--- OoD_problem/Massimo/main.py
+++ OoD_problem/Massimo/main.py
@@ -1,7 +1,7 @@
 from read_xml import read_xml
 from time import clock
 # create a structure reading the input data
-struct = read_xml("xml_files/2_3.xml")
+struct = read_xml("xml_files/2_2_c1.xml")
 
 t1 = clock()
 
@@ -12,4 +12,4 @@ t2 = clock()
 print("Total time running struct.solve: %s seconds" % str(t2-t1))
 
 # output the solution
-print(struct)
+struct.print_solution()
