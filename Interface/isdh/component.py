13827e0268e2ed38f3018f58587bc4be4c39c9bd
IIIaxS <msferza@gmail.com>
2016-06-19 08:25:16 +0200

    isdh connections

--- Interface/isdh/component.py
+++ Interface/isdh/component.py
@@ -1,3 +1,4 @@
+class 
 class Component:
     def __init__(self, name, x1, x2, defo_length, lp_level):
         # initial state
@@ -6,3 +7,20 @@ class Component:
         self.x2 = x2
         self.defo_length = defo_length
         self.lp_level = lp_level
+        
+    def move():
+        pass
+
+class Connection:
+    def __init__(self, name, x1, x2, defo_length, end_level):
+        # initial state
+        self.name = name
+        self.x1 = x1
+        self.x2 = x2
+        self.defo_length = defo_length
+        self.lp_level = lp_level        
+        self.end_level = end_level
+
+    def move():
+        pass# other way
+
--- Interface/isdh/deformation_step.py
+++ Interface/isdh/deformation_step.py
@@ -3,7 +3,7 @@ class DeformationStep:
         self.amount = amount
         self.frame_begin = initial_deformation_amount
         self.frame_end = initial_deformation_amount + amount
-        self.transformation = transformation # 'm' or 'd'
+        self.transformation = transformation # 'm' or 'd' or 'b'
 ##        # LEGEND:
 ##        # 'd': the element deforms
 ##        # 'm': the element moves
