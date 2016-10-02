9f477ffb70edd2defdde9c5e9392a3c78a17d732
IIIaxS <msferza@gmail.com>
2016-10-02 11:08:35 +0200

    style change
    PEP8

--- TaskOne/Node.py
+++ TaskOne/Node.py
@@ -5,15 +5,20 @@ logging.basicConfig(level=logging.DEBUG)
 
 class Node():
     
-    def __init__(self,point):
-       #Location of the node at the respective loadpath
+    def __init__(self, point):
+        # Location of the node at the respective loadpath
         self.position = point
-        #Level of loadpath the node belongs to. It has a default value, and it will be changed by its members
+        # Level of loadpath the node belongs to. It has a default value, and it
+        # will be changed by its members
         self.loadpathLevel = 0
-        #List contains all members that are connected through the node object
+        # List contains all members that are connected through the node object
         self.connectingMembers = []
 
-    #Method that acts on the node's position attribute to change it from one place to another
-    def changePosition(self, deformationStep):
-        logger.debug("node at position {} and loadpath {} has changed its position by {}".format(self.position,self.loadpathLevel,x))
+    # Method that acts on the node's position attribute to change it from one
+    # place to another
+    def change_position(self, deformationStep):
+        message = "node at position {} and loadpath {} has changed its \
+position by {}"
+        logger.debug(message.format(self.position, self.loadpathLevel, x))
+        
         self.position += deformationStep
