339a23d77cb63b2c5dd47f6840338d4eefe3f339
moustafabachar <m.b.alsayedahmad@gmail.com>
2016-10-03 19:19:27 +0200

    Correction of some of the attributes

--- TaskOne/Loadpath.py
+++ TaskOne/Loadpath.py
@@ -1,15 +1,12 @@
-import member
-
 class Loadpath():
 '''
 Class contains it index ,all of the components within the loadpath
 , and counts number of components not able to defrom
 anymore.
 '''
-  def __init__(self, n):
-    self.loadpathNumber = n 
+  def __init__(self):
     self.listOfComponents = [ ] 
-    self.noOfComponents= 0 
+    self.noComponentsNotDeformable= 0 
 
   def add_member(self, component):
     '''
@@ -17,18 +14,18 @@ anymore.
     '''
     self.listOfComponents.append(component)
 
-  def increase_no_of_components(self):
+  def increase_noComponentsNotDeformable(self):
     '''
     Function increases number of components not able to deform
     permenantly by one
     '''
-    self.noOfComponents += 1
-  def decrease_no_of_components(self):
+    self.noComponentsNotDeformable += 1
+  def decrease_noComponentsNotDeformable(self):
     '''
     Function decreases number of components not able to defrom
     permenantly by one
     '''
-    self.noOfComponents -= 1
+    self.noComponentsNotDeformable -= 1
 
   
 
